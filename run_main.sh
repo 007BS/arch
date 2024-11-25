be9a58259edfe38f61a304a1447f27b9d84b2d3b
007BS <Kwartelslagen@gmail.com>
2024-11-25 20:09:18 +0100

    Add files via upload

--- run_main.sh
+++ run_main.sh
@@ -1,1 +1,2 @@
-﻿bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) "<(curl -L 'https://raw.githubusercontent.com/007BS/arch/refs/heads/Arch/config.sh')"
+﻿echo 100
+bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) "test"
