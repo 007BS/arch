66481759b885647f188e6ce7070e5d9f5cc1eaa0
007BS <Kwartelslagen@gmail.com>
2024-11-25 19:49:58 +0100

    Add files via upload

--- config.sh
+++ config.sh
@@ -53,4 +53,4 @@ App_Install__vuze=1
 App_Install__waterfox=1
 App_Install__wire=1
 
-<(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh)
+#bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) "<(curl -L 'https://raw.githubusercontent.com/007BS/arch/refs/heads/Arch/config.sh')"
