995ed107edf67b65220f3aa88f6c3332671687eb
007BS <Kwartelslagen@gmail.com>
2024-11-26 14:31:52 +0100

    Add files via upload

--- run_main.sh
+++ run_main.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) "https://raw.githubusercontent.com/007BS/arch/refs/heads/Arch/config.sh"
---
+++ run_main_local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) ~/Desktop/config.sh
