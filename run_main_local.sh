a927237bf5a469e8421bf35190ba880676b417ea
007BS <Kwartelslagen@gmail.com>
2024-11-26 14:41:27 +0100

    Add files via upload

--- run_main_local.sh
+++ run_main_local.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# Disclaimer:
+# This script is provided as-is, without any warranty or guarantee.
+# By using this script, you acknowledge that you do so at your own risk.
+# I am not responsible for any damage, data loss, or other issues that may result from the use of this script.
 
 bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) ~/Desktop/config.sh
