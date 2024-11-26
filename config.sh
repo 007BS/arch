5037edcbcdc1a1e94307d8557bc47de80110f905
007BS <Kwartelslagen@gmail.com>
2024-11-26 15:56:52 +0100

    Add files via upload

--- config.sh
+++ config.sh
@@ -4,31 +4,43 @@
 # By using this script, you acknowledge that you do so at your own risk.
 # I am not responsible for any damage, data loss, or other issues that may result from the use of this script.
 
+#Archives
+App_Install__7_zip=1
+
 #browsers
 App_Install__brave=1
 App_Install__firefox=1
+App_Install__google_chrome=1
+App_Install__opera=0
+App_Install__thorium=1
+App_Install__torbrowser=0
+App_Install__waterfox=1
+
+#game launchers
+App_Install__heroic_launcher=1
+App_Install__paradox_launcher=1
+
+#Terminals
 
 #themes
 App_Install__adwaita_theme=1
+App_Install__font_dejavu=1
+
 
 App_Install__wine=1
 App_Install__wine_mono=1
-App_Install__7_zip=1
-App_Install__anydesk=1
-App_Install__audacity=1
+App_Install__anydesk=0
+App_Install__audacity=0
 App_Install__biglybt=1
 App_Install__bleachbit=1
-App_Install__calibre=1
+App_Install__calibre=0
 App_Install__discord=1
-App_Install__dropbox=1
+App_Install__dropbox=0
 App_Install__filezilla=1
-App_Install__font_dejavu=1
 App_Install__git=1
-App_Install__gimp=1
+App_Install__gimp=0
 App_Install__gnome_terminal=1
-App_Install__google_chrome=1
 App_Install__handbrake=1
-App_Install__heroic_launcher=1
 App_Install__jitsi_meet=1
 App_Install__keepass=1
 App_Install__libreoffice=1
@@ -37,24 +49,19 @@ App_Install__minecraft_launcher=1
 App_Install__notepadqq=1
 App_Install__notepadPlusPlus=1
 App_Install__numlockx=1
-App_Install__obs_studio=1
-App_Install__opera=1
-App_Install__paradox_launcher=1
+App_Install__obs_studio=0
 App_Install__pcloud=1
 App_Install__peazip=1
-App_Install__pidgin=1
+App_Install__pidgin=0
 App_Install__scrcpy=1
-App_Install__session=1
-App_Install__signal=1
+App_Install__session=0
+App_Install__signal=0
 App_Install__steam=1
 App_Install__teamviewer=1
-App_Install__thorium=1
-App_Install__torbrowser=1
 App_Install__thunderbird=1
 App_Install__tigervnc=1
 App_Install__virtualbox=1
-App_Install__visual_studio_code=1
+App_Install__visual_studio_code=0
 App_Install__vlc=1
-App_Install__vuze=1
-App_Install__waterfox=1
+App_Install__vuze=0
 App_Install__wire=1
