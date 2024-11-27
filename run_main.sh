d2d672c860b2255b75e142c8a2cac1a49078ec35
007BS <Kwartelslagen@gmail.com>
2024-11-27 20:26:40 +0100

    Add files via upload

--- run_main.sh
+++ run_main.sh
@@ -5,3 +5,5 @@
 # I am not responsible for any damage, data loss, or other issues that may result from the use of this script.
 
 bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) "https://raw.githubusercontent.com/007BS/arch/refs/heads/Arch/config.sh" "https://raw.githubusercontent.com/007BS/arch/refs/heads/Arch/config_after.sh" | bash -s -- && source ~/.bashrc
+
+echo jippie
