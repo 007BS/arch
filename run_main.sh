cd14055b7bc665511b5875dce5ece86d27bdb33d
007BS <Kwartelslagen@gmail.com>
2024-11-27 17:01:27 +0100

    Add files via upload

--- run_main.sh
+++ run_main.sh
@@ -4,4 +4,4 @@
 # By using this script, you acknowledge that you do so at your own risk.
 # I am not responsible for any damage, data loss, or other issues that may result from the use of this script.
 
-bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) "https://raw.githubusercontent.com/007BS/arch/refs/heads/Arch/config.sh"
+bash <(curl -L https://raw.githubusercontent.com/TheSuperGiant/Arch/refs/heads/Arch/2.sh) "https://raw.githubusercontent.com/007BS/arch/refs/heads/Arch/config.sh" "https://raw.githubusercontent.com/007BS/arch/refs/heads/Arch/config_after.sh"
