d56ca553e45b7d3039d3a76784383ee5f4b2555a
007BS <Kwartelslagen@gmail.com>
2024-12-12 15:32:54 +0100

    Add files via upload

--- config.sh
+++ config.sh
@@ -5,22 +5,22 @@
 # I am not responsible for any damage, data loss, or other issues that may result from the use of this script.
 
 #Archives
-#App_Install__7_zip=1
+App_Install__7_zip=1
 
 #browsers
-#App_Install__brave=1
-#App_Install__firefox=1
-#App_Install__google_chrome=0
-#App_Install__librewolf=1
-#App_Install__opera=0
-#App_Install__thorium=1
-#App_Install__torbrowser=0
-#App_Install__waterfox=1
+App_Install__brave=1
+App_Install__firefox=1
+App_Install__google_chrome=0
+App_Install__librewolf=1
+App_Install__opera=0
+App_Install__thorium=1
+App_Install__torbrowser=0
+App_Install__waterfox=1
 
 #game launchers
-#App_Install__heroic_launcher=1
-#App_Install__minecraft_launcher=1
-#App_Install__paradox_launcher=1
+App_Install__heroic_launcher=1
+App_Install__minecraft_launcher=1
+App_Install__paradox_launcher=1
 
 #Terminals
 App_Install__gnome_terminal=1
@@ -33,43 +33,43 @@ App_Install__font_dejavu=1
 
 App_Install__wine=1
 App_Install__wine_mono=1
-#App_Install__anydesk=0
-#App_Install__audacity=0
-#App_Install__biglybt=1
-#App_Install__bleachbit=1
-#App_Install__calibre=0
-#App_Install__discord=1
-#App_Install__dropbox=0
-#App_Install__filezilla=1
-#App_Install__git=1
-#App_Install__gimp=0
-#App_Install__handbrake=1
-#App_Install__jitsi_meet=0
-#App_Install__keepass=1
-#App_Install__leafpad=1
-#App_Install__libreoffice=1
-#App_Install__mega=1
-#App_Install__mousepad=0
-#App_Install__notepadqq=1
-#App_Install__notepadPlusPlus=1
-#App_Install__numlockx=1
-#App_Install__obs_studio=0
-#App_Install__pcloud=1
-#App_Install__peazip=1
-#App_Install__pidgin=0
-#App_Install__scrcpy=1
-#App_Install__session=0
-#App_Install__signal=0
-#App_Install__steam=1
-#App_Install__teamviewer=1
-#App_Install__thunderbird=1
-#App_Install__tigervnc=1
-#App_Install__virtualbox=1
-#App_Install__visual_studio_code=0
-#App_Install__vlc=1
-#App_Install__vuze=0
-#App_Install__wire=1
-#App_Install__xed=0
+App_Install__anydesk=0
+App_Install__audacity=0
+App_Install__biglybt=1
+App_Install__bleachbit=1
+App_Install__calibre=0
+App_Install__discord=1
+App_Install__dropbox=0
+App_Install__filezilla=1
+App_Install__git=1
+App_Install__gimp=0
+App_Install__handbrake=1
+App_Install__jitsi_meet=0
+App_Install__keepass=1
+App_Install__leafpad=1
+App_Install__libreoffice=1
+App_Install__mega=1
+App_Install__mousepad=0
+App_Install__notepadqq=1
+App_Install__notepadPlusPlus=1
+App_Install__numlockx=1
+App_Install__obs_studio=0
+App_Install__pcloud=1
+App_Install__peazip=1
+App_Install__pidgin=0
+App_Install__scrcpy=1
+App_Install__session=0
+App_Install__signal=0
+App_Install__steam=1
+App_Install__teamviewer=1
+App_Install__thunderbird=1
+App_Install__tigervnc=1
+App_Install__virtualbox=1
+App_Install__visual_studio_code=0
+App_Install__vlc=1
+App_Install__vuze=0
+App_Install__wire=1
+App_Install__xed=0
 
 add_device_labels=("Data" "Games")
 
